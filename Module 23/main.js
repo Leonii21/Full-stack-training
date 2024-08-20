9e6133052236a6e407458d742a88863a4fbbf323
Leonii21 <jasharileon591@gmail.com>
2024-08-20 18:29:31 +0200

    Moduli 23

---
+++ Module 23/main.js
@@ -0,0 +1,35 @@
+// //FUNCTION
+
+// function showMessage(){
+//     alert("This message is inside a function")
+// }
+// showMessage()
+
+function sum(number1,number2){
+    return number1 + number2
+}
+console.log(sum(30,45))
+
+function toCelsius(f){
+    return (5/9) * (f-32)
+}
+console.log("54 fahrenheit is equal to" +toCelsius(54)+ "celsius")
+
+var result =toCelsius(54)
+console.log("54 fahrenheit is equal to " +  result+ "celsius")
+
+function ds(){
+    var localVar="Digital School"
+    alert(localVar)
+}
+ds()
+ 
+
+//alert(localvar)e deklarume brenda funksionit
+
+function toSeconds(minutes){
+    return minutes* 60
+
+}
+console.log(toSeconds(60))
+    
