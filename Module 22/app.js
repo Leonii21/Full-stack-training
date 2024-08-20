d58db285d3f6f1fafe98731e36cb7f97ae586e31
Leonii21 <jasharileon591@gmail.com>
2024-08-20 18:05:23 +0200

    Moduli 22

---
+++ Module 22/app.js
@@ -0,0 +1,99 @@
+console.log("Good Job Guys")
+
+var x=5
+
+// var 123 = 10 ; nuk duhet me i emertu variablat me numra
+// var *ds = 45; nuk duhet me ja nis me special charachters
+
+
+var a=50 + 50
+console.log(a)
+
+var b=10
+var c=20
+var d=b+c
+console.log(d)
+
+var g= c-b 
+console.log(g)
+
+var k=10
+var l= 4;
+var m=k % 1
+console.log(m)
+
+var n = 99
+// n++  preffix 
+console.log(n)
+
+var o = 101
+o -- 
+console.log(o)
+
+var p = 5
+var q = 2 
+var h = p**q
+console.log(h)
+
+
+var e = 15
+e +=5
+console.log(e)
+
+e-=5
+console.log(e)
+
+/// /////////////////////////////////////////
+
+
+var u=10
+u*=3
+console.log(u)
+
+
+u/=2
+console.log(u)
+
+///////////////////////////////////////
+
+var t=5
+var i=5
+console.log(t==i) /// true
+
+
+var n1= 6
+var n2= 7
+console.log(n1!= n2) ////// true 
+
+console.log(n1>n2) ////// false 
+console.log(n1<n2)////// true
+
+
+
+var numri=123
+var teksti="123"
+console.log(numri === teksti)////// false
+
+var firstname="Digital"
+var lastname="School"
+var fullname = firstname + " "+ lastname
+console.log(fullname)
+
+
+var school="Shkolla digjitale"
+console.log('hello from ${school}')
+
+//And operator - Duhet me plotsu krejt kushtet qe me dal shprehja true
+
+var age=24
+console.log(age>=18 && age<=65)
+
+
+// Or operator-vetem nese nje kusht true,shprehja na kthen true 
+var numri1=5
+var numri2=6
+console.log(numri1==5 || numri2==5)
+
+var num3=9
+var num4=10
+console.log(!(num3==num4))
