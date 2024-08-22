712bcafcc4fb782cb3b46abc617a705a2cc69090
Leon <jasharileon591@gmail.com>
2024-08-22 18:31:19 +0200

    Module 24

---
+++ Moduli 24/main.js
@@ -0,0 +1,170 @@
+// var button1 = document.getElementById("btn1");
+
+// button1.onclick = function(){
+//     alert("Hello World!");
+
+// }
+ var button1 = document.querySelector("button");
+var button2 = document.getElementById("btn2");
+var button3 = document.getElementById("btn3");
+var button4 = document.getElementById("btn4");
+var btnhide = document.getElementById("btnHide");
+
+
+btnhide.onclick = function(){
+ btnhide.setAttribute("class","test2");
+}
+
+
+ button1.addEventListener("click",function(){
+     alert("Hello Guyss");
+ })
+
+button1.setAttribute("class","test");
+
+
+button3.onmouseleave=function(){
+    alert("Une jam filan fisteku");
+}
+button4.onmouseenter=function(){
+    alert("Pershendetje nga baba");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// button2.onmouseover=function(){
+//     alert("Une jom buton 2shi");
+// }button2.onmouseover=function(){
+    // alert("Une jom buton 2shi");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// button2.onmouseover=function(){
+//     alert("Une jom buton 2shi");
+// }button2.onmouseover=function(){
+    // alert("Une jom buton 2shi");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var v_button = document.getElementById("btn1");
+// v_button.onclick = function(){
+//     v_button.style.color ="red";
+//     v_button.style.backgroundColor ="blue";
+//     v_button.style.textAlign ="center";
+//     v_button.style.fontSize ="100px";
+//     v_button.style.padding = "20px";
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var v_button = document.getElementById("btn1");
+// v_button.onclick = function(){
+//     v_button.style.color ="red";
+//     v_button.style.backgroundColor ="blue";
+//     v_button.style.textAlign ="center";
+//     v_button.style.fontSize ="100px";
+//     v_button.style.padding = "20px";
+
+// }
+
