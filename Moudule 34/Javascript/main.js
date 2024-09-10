02e29c309d1ce3c126974f4ad2b9478002be3d45
Leonii21 <jasharileon591@gmail.com>
2024-09-10 17:02:25 +0200

    Module 34

---
+++ Moudule 34/Javascript/main.js
@@ -0,0 +1,14 @@
+$('#sort').sortable();
+
+
+$(function(){
+    $(" #accordion" ).accordion({
+        collapsible:true
+    });
+})
+
+$(function() {
+    $("#tabs").tabs({
+        collapsible:true
+    });
+});
